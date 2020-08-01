38de891fc1b1b3dde090013a2cbd747c9ac9a93c
Rails udemy course <Railsudemycourse@gmail.com>
2020-08-01 22:48:09 +0530

    Changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,4 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :item_price, presence: true 
   validates :user_id, presence: true
-  validates :bill_id, presence: true
 end
